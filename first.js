f99c993162d52ca76868f1d3a7014e18b4da21bd
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-05 16:17:59 +0600

    file setup and code initialize

---
+++ first.js
@@ -0,0 +1,7 @@
+var myTaka = 500;
+var bananaPrice = 250;
+var orangePrice = 200;
+var total = bananaPrice + orangePrice;
+console.log(total)
+var nowMyTaka =( myTaka - total);
+console.log(nowMyTaka);
