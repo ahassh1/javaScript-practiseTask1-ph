bdd62d9fae8fda453326c049cacd205c26b2b1b8
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-08 19:14:25 +0600

    checking an array

--- array_checking.js
+++ array_checking.js
@@ -6,3 +6,10 @@
 // Now use isArray to check if each variable is an array.
 
 // Print a message to the console indicating whether each variable is an array or not.
+
+var name = ['polash', 'habib','ahasan', 'mohammad'];
+var num = [];
+var age = [20, 21, 22, 23];
+console.log(Array.isArray("name"))
+console.log(Array.isArray(num))
+console.log(Array.isArray(age))
