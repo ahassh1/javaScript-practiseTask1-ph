f615fb29baaaca56f5ffc26c6d84d3170e47040c
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-08 18:49:47 +0600

    array_combining.js

--- array_combining.js
+++ array_combining.js
@@ -4,5 +4,7 @@
 // Create two arrays of your choice.
 // Use the concat method to combine the two arrays into a new array.
 // Print both the original arrays and the combined array using console.log().
-var numbers [10,20,30];
-var 
+var numbers1 = [10,20,30];
+var numbers2 = [40,60,90];
+var wholeNumber = numbers1.concat(numbers2);
+console.log(wholeNumber)
