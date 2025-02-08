31f608b75053da43ed420cba5bb45b9cef70bf70
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-08 17:56:04 +0600

    ternary js done

--- ternary.js
+++ ternary.js
@@ -8,3 +8,14 @@ write a simple if-else.
 
 also, write it using ternary operator.
  */
+var num1 =10;
+var num2 =20;
+var result =num1 + num2;
+if(num1>num2){
+    console.log(num1*2)
+}
+else{
+    console.log(num1+num2)
+}
+
+num1>num2 ? console.log (num1*2) : console.log (num1+num2)
